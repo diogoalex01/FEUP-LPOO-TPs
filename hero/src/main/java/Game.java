922391259fc24231b81bc02ae99d37119fb9fca9
mafaldasantos10 <anamaf13@gmail.com>
2019-02-26 13:37:56 +0000

    7 - Reading Keystrokes

--- hero/src/main/java/Game.java
+++ hero/src/main/java/Game.java
@@ -1,3 +1,6 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -8,19 +11,64 @@ import java.io.IOException;
 public class Game {
 
     private Screen screen;
+    private int x =10;
+    private int y =10;
 
     public Game() throws IOException {
             Terminal terminal = new DefaultTerminalFactory().createTerminal();
             screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);   // we don't need a cursor
+        screen.startScreen();             // screens must be started
+        screen.doResizeIfNecessary();     // resize screen if necessary
     }
 
     private void draw() throws IOException {
-            screen.setCursorPosition(null);   // we don't need a cursor
-            screen.startScreen();             // screens must be started
-            screen.doResizeIfNecessary();     // resize screen if necessary
+            screen.clear();
+            screen.setCharacter(x, y, new TextCharacter('X'));
+            screen.refresh();
     }
 
     public void run () throws IOException {
-        draw();
+
+        KeyStroke key;
+
+        do {
+
+            draw();
+            key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
+                screen.close();
+            }
+            if (key.getKeyType() == KeyType.EOF){
+                break;
+            }
+            processKey(key);
+        } while ( true);
+    }
+
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+
+        switch (key.getKeyType()) {
+            case ArrowUp:
+            {
+                y--;
+                break;
+            }
+            case ArrowLeft:
+            {   x--;
+                break;
+            }
+            case ArrowDown:
+            {
+                y++;
+                break;
+            }
+            case ArrowRight:
+            {
+                x++;
+                break;
+            }
+        }
     }
 }
